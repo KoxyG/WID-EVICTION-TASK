60f21c519517ca35674825392de96d9119b8253f
Rawan-tech103 <rawan_2443a@pt.kfs.edu.eg>
2025-08-08 20:27:52 +0300

    my-solution

--- oop/task2.js
+++ oop/task2.js
@@ -15,4 +15,24 @@
  * 
  * Bonus Points: Attach screenshot of proof it works on your terminal.
  */
+class Car {
+    constructor(make, model, year) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+
+    
+    getCarInfo() {
+        return `Make: ${this.make}, Model: ${this.model}, Year: ${this.year}`;
+    }
+}
+
+
+const car1 = new Car("Toyota", "Corolla", 2020);
+const car2 = new Car("Tesla", "Model S", 2023);
+
+
+console.log(car1.getCarInfo());
+console.log(car2.getCarInfo());
 
